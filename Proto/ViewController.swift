32a9cfdaecbf86812e0d60184693fb57644fd457
Jaehyoen <41604592+Jaehyoen@users.noreply.github.com>
2018-07-24 13:04:57 +0900

    Add files via upload

---
+++ Proto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proto
+//
+//  Created by CAUAD11 on 2018. 7. 11..
+//  Copyright © 2018년 CAUAD11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
